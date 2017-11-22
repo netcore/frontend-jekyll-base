4431100a771ec5d4ba448fbd22eedd56188860a2
Vladislavs Žizņevskis <vladislavs.ziznevskis@netcore.lv>
2017-11-22 18:38:07 +0200

    Disabled some of unnecessary bootstrap JS/SASS components and fixed indentation conflicts

--- resources/_assets/js/app.js
+++ resources/_assets/js/app.js
@@ -2,12 +2,14 @@
 let baseHref = window.location.origin
 let $base = $('base')
 if ($base.length)
-    baseHref = $base.prop('href')
+	baseHref = $base.prop('href')
 global.baseUrl = baseHref.substr(-1) === '/' ? baseHref.slice(0, baseHref.length - 1) : baseHref
+
+// imports
+// ...
 
 // The no-js class is provided in order to allow you to more easily and explicitly add custom styles based
 // on whether JavaScript is disabled (.no-js) or enabled (.js).
 $('html.no-js').removeClass('no-js').addClass('js')
 
 // ...
-console.log('it works!')
