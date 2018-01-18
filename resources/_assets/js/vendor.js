f29c50c9bc3a2926c1aa1e076f3143e7f9f11467
Dāvis Pabērzs <paberzs.davis@gmail.com>
2018-01-18 19:04:43 +0200

    Remove old code

--- resources/_assets/js/vendor.js
+++ resources/_assets/js/vendor.js
@@ -1,10 +1,6 @@
 import jQuery from 'jquery'
 global.$ = global.jQuery = jQuery
 
-// add vue
-import 'vue'
-global.Vue = Vue
-
 // import bootstrap components
 import 'bootstrap/js/dist/util'
 import 'bootstrap/js/dist/alert'
